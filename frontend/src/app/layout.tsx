c3f6fb1412cb1645d99ef7e7ad7ee2ceddf491d8
Vinícius Dutt <vinidutra13@gmail.com>
2026-08-05 08:58:06 -0300

    fix: adsense script

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -44,11 +44,10 @@ export default function RootLayout({
           <Sunbeam />
         </div>
         {children}
-        <Script
-          id="google-adsense"
+        <script
+          async
           src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_CLIENT_ID}`}
           crossOrigin="anonymous"
-          strategy="afterInteractive"
         />
       </body>
     </html>
